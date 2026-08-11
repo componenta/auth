037ddbbc30f62cfc5eeba7bc5d90dbee49addc48
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-11 22:13:43 +0200

    feat(auth): wire database refresh store factory

---
+++ src/Factory/DatabaseRefreshTokenStoreFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Componenta\Auth\Factory;
+
+use Componenta\Auth\Http\Strategy\Jwt\DatabaseRefreshTokenStore;
+use Componenta\Auth\Http\Strategy\Jwt\DatabaseRefreshTokenStoreConfig;
+use Cycle\Database\DatabaseInterface;
+use Psr\Container\ContainerInterface;
+
+final readonly class DatabaseRefreshTokenStoreFactory
+{
+    public function __invoke(
+        ContainerInterface $container,
+    ): DatabaseRefreshTokenStore {
+        $database = $container->get(DatabaseInterface::class);
+        $config = $container->get(DatabaseRefreshTokenStoreConfig::class);
+
+        if (!$database instanceof DatabaseInterface) {
+            throw new \LogicException(sprintf(
+                '%s must resolve to %s.',
+                DatabaseInterface::class,
+                DatabaseInterface::class,
+            ));
+        }
+
+        if (!$config instanceof DatabaseRefreshTokenStoreConfig) {
+            throw new \LogicException(sprintf(
+                '%s must resolve to %s.',
+                DatabaseRefreshTokenStoreConfig::class,
+                DatabaseRefreshTokenStoreConfig::class,
+            ));
+        }
+
+        return new DatabaseRefreshTokenStore($database, $config);
+    }
+}
