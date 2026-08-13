bae08da8368ca98be6260e1f71b69528337def6f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-13 22:24:33 +0200

    test(http): cover explicit empty token responses

--- tests/Http/Strategy/Jwt/TokenResponseHeadersTest.php
+++ tests/Http/Strategy/Jwt/TokenResponseHeadersTest.php
@@ -7,33 +7,28 @@ namespace Componenta\Auth\Tests\Http\Strategy\Jwt;
 use Componenta\Auth\Http\Strategy\Jwt\TokenResponseHeaders;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\StreamInterface;
 
 final class TokenResponseHeadersTest extends TestCase
 {
-    public function testEmptyBodyDoesNotClaimJsonContentType(): void
+    public function testJsonResponseDoesNotInferSemanticsFromBodySize(): void
     {
-        $body = $this->createStub(StreamInterface::class);
-        $body->method('getSize')->willReturn(0);
         $response = $this->createMock(ResponseInterface::class);
-        $response->method('getBody')->willReturn($body);
+        $response->expects(self::never())->method('getBody');
+        $response->expects(self::exactly(3))->method('withHeader')->willReturnSelf();
+        $response->expects(self::never())->method('withoutHeader');
+
+        self::assertSame($response, TokenResponseHeaders::apply($response));
+    }
+
+    public function testEmptyResponseDoesNotInferSemanticsFromBodySize(): void
+    {
+        $response = $this->createMock(ResponseInterface::class);
+        $response->expects(self::never())->method('getBody');
         $response->expects(self::exactly(2))->method('withHeader')->willReturnSelf();
         $response->expects(self::once())->method('withoutHeader')
             ->with('Content-Type')
             ->willReturnSelf();
 
-        self::assertSame($response, TokenResponseHeaders::apply($response));
-    }
-
-    public function testNonEmptyBodyKeepsJsonContentType(): void
-    {
-        $body = $this->createStub(StreamInterface::class);
-        $body->method('getSize')->willReturn(2);
-        $response = $this->createMock(ResponseInterface::class);
-        $response->method('getBody')->willReturn($body);
-        $response->expects(self::exactly(3))->method('withHeader')->willReturnSelf();
-        $response->expects(self::never())->method('withoutHeader');
-
-        self::assertSame($response, TokenResponseHeaders::apply($response));
+        self::assertSame($response, TokenResponseHeaders::applyEmpty($response));
     }
 }
